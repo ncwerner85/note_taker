5b874e2ea0c029582faca1c1e8ecaac56beba254
Nicholas Werner <ncwerner@gmail.com>
2020-12-19 08:44:45 -0700

    added server

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -5,7 +5,8 @@ const notes = require("../db/db.json");
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
     res.json(notes);
-  });
+});
+
 app.post("/api/notes", function (req, res) {
     const newNotes = req.body;
 
@@ -22,24 +23,5 @@ app.post("/api/notes", function (req, res) {
           res.json(true);
         }
       );
-});
-
-//   app.delete("/api/clear", function (req, res) {
-//     fs.writeFile(
-//       path.join(__dirname, "../db/waitlist.json"),
-//       JSON.stringify([]),
-//       function (err) {
-//         if (err) throw err;
-//       }
-//     );
-//     fs.writeFile(
-//       path.join(__dirname, "../db/tables.json"),
-//       JSON.stringify([]),
-//       function (err) {
-//         if (err) throw err;
-
-//         res.json("cleared!");
-//       }
-//     );
-//   });
-// };
+    }
+});    
